381f62f84d177aaf1cd8398c5f93f6be1a4c8489
Felipemacedo315 <felipemacedo315@gmail.com>
2021-08-06 12:26:18 -0300

    Revert "Update produtos.jsx"
    This reverts commit 7182ac124d294309c25da2b5703da26d2391571c.

--- src/produtos.jsx
+++ src/produtos.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react'
 import { Buttoncomprar } from './globalestilos'
 import { Link } from 'react-router-dom'
 
-import { Paginacompra1,Paginacompra2,Paginacompra3,Paginacompra4,Paginacompra5,Paginacompra6, Paginacompra8 } from './paginacompra'
-
  
 
   export  function Produto1() {
@@ -13,9 +11,9 @@ import { Paginacompra1,Paginacompra2,Paginacompra3,Paginacompra4,Paginacompra5,P
         <>
       
         <div className='cada-produto'>
-    <img className='imgproduto' src='/imagens\charuto-bolivar-belicoso-fino.jpg'/>
+    <img src='/imagens\charuto-bolivar-belicoso-fino.jpg'/>
     <h2>Charuto Vegafina Vintage 1998 42 </h2>
-    <p className='preço'>R$ {charuto1}</p>
+    <p>R$ {charuto1}</p>
     <input type='number' min='0'/>
  
    
@@ -29,9 +27,9 @@ import { Paginacompra1,Paginacompra2,Paginacompra3,Paginacompra4,Paginacompra5,P
     const[charuto2,setcharuto2]=useState(129.99 )
     return(
         <div className='cada-produto'>
-    <img className='imgproduto' src='/imagens\charuto-bolivar-royal.jpg'/>
+    <img src='/imagens\charuto-bolivar-royal.jpg'/>
     <h2>Charuto Perceverancia Serie Artesano Gran Habano 60 </h2>
-    <p className='preço'>R$ {charuto2}</p>
+    <p>R$ {charuto2}</p>
     <input type='number' min='0'/>
   
         </div>
@@ -44,9 +42,9 @@ import { Paginacompra1,Paginacompra2,Paginacompra3,Paginacompra4,Paginacompra5,P
     const[charuto3,setcharuto3]=useState(39.99 )
     return(
         <div className='cada-produto'>
-    <img className='imgproduto' src='/imagens\charuto-bolivar.jpg'/>
+    <img src='/imagens\charuto-bolivar.jpg'/>
     <h2>Charuto Perceverancia Premium Gran Figurado </h2>
-    <p className='preço'>R$ {charuto3}</p>
+    <p>R$ {charuto3}</p>
     <input type='number' min='0'/>
   
 
@@ -58,9 +56,9 @@ import { Paginacompra1,Paginacompra2,Paginacompra3,Paginacompra4,Paginacompra5,P
     const[charuto4,setcharuto4]=useState(69.99 )
     return(
         <div className='cada-produto'>
-    <img className='imgproduto' src='/imagens\charuto-cohiba.jpg'/>
+    <img src='/imagens\charuto-cohiba.jpg'/>
    <h2>Charuto Romeu e Julieta Petit Churchill</h2>
-    <p className='preço'>R$ {charuto4}</p>
+    <p>R$ {charuto4}</p>
     <input type='number' min='0'/>
   
         </div>
@@ -71,9 +69,9 @@ export function Produto5 (){
     const[charuto5,setcharuto5]=useState(299.99 )
     return(
         <div className='cada-produto'>
-    <img className='imgproduto' src='/imagens\charuto-coronitas.jpg'/>
+    <img src='/imagens\charuto-coronitas.jpg'/>
     <h2>Charuto Alonso Menendez Del Patron Gran Corona</h2>
-    <p className='preço'>R$ {charuto5}</p>
+    <p>R$ {charuto5}</p>
     <input type='number' min='0'/>
   
         </div>
@@ -84,9 +82,9 @@ export function Produto6 (){
     const[charuto6,setcharuto6]=useState(189.99 )
     return(
         <div className='cada-produto'>
-    <img className='imgproduto' src='/imagens\charuto-don-diego.jpg'/>
+    <img src='/imagens\charuto-don-diego.jpg'/>
     <h2>Charuto Monte Pascoal Short Robusto</h2>
-    <p className='preço'>R$ {charuto6}</p>
+    <p>R$ {charuto6}</p>
     <input type='number' min='0'/>
  
         </div>
@@ -97,9 +95,9 @@ export function Produto7 (){
     const[charuto7,setcharuto7]=useState(174.99 )
     return(
         <div className='cada-produto'>
-    <img className='imgproduto' src='/imagens\charuto-dondiego-belicoso.jpg'/>
+    <img src='/imagens\charuto-dondiego-belicoso.jpg'/>
    <h2>Charuto Dona Flor Seleção Robusto</h2>
-    <p className='preço'>R$ {charuto7}</p>
+    <p>R$ {charuto7}</p>
     <input type='number' min='0'/>
    
         </div>
@@ -108,25 +106,12 @@ export function Produto7 (){
 }
 export function Produto8 (){
     const[charuto8,setcharuto8]=useState(219.99 )
-    const[quantidade,setquantidade]=useState('')
-  
-  function capturar_quantidade8(e){
-      var quantidadevar = e.target.value
-    setquantidade(quantidadevar*charuto8)
-    window.alert(quantidade)
-      
-   }
-
     return(
-        
         <div className='cada-produto'>
-          
-         
-       
-    <img className='imgproduto' src='/imagens\charuto-mille-feur.jpg'/>
+    <img src='/imagens\charuto-mille-feur.jpg'/>
     <h2>Charuto Los 3 Catedraticos Magnífico Maduro (Canonazo)</h2>
-    <p className='preço'>R$ {charuto8}</p>
-    <input onChange={capturar_quantidade8} type='number' min='0'/>
+    <p>R$ {charuto8}</p>
+    <input type='number' min='0'/>
     
         </div>
 
